5e4940c32cdbe9d9eea1ecb2467066514bf093d1
RafaOliv22 <rafael_gbh@hotmail.com>
2023-11-20 21:09:47 -0300

    factory e seeder funcionando

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,17 @@
+<?php
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        User::create([
+            'username' => 'administrador2',
+            'password' => Hash::make('teste'),
+        ]);
+    }
+}
